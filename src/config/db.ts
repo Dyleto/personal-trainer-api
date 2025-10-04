87edcde523680c47332e1e13e5902809b7ba09df
Dyleto <corentinlemoullec@gmail.com>
2025-10-04 23:27:28 +0200

    passage en TS

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI || "");
     console.log("✅ MongoDB connected");
   } catch (error) {
-    console.error("❌ MongoDB connection error:", error.message);
+    console.error("❌ MongoDB connection error:", error);
     process.exit(1);
   }
 };
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./src/config/db.js";
 import routes from "./src/routes/index.js";
 
 dotenv.config();
 const app = express();
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173", // front dev
+      "https://personal-trainer-fynm264op-dyletos-projects.vercel.app", // front prod
+    ],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
